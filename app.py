f70c64bda9ed79c0bf55138abd7b466244e02dfe
kojiro0208 <h.wadsworth02@gmail.com>
2023-07-09 11:19:01 +0900

    修正

--- app.py
+++ app.py
@@ -246,4 +246,4 @@ import sys
 
 
 def handler(event, context):
-    return "Hello from AWS Lambda using Python" + sys.version + "!"
+    return "Hello from AWS Lambda using Python" + sys.version + "!!"
